146ee2a072a7ed10a0a50d498d319654e7cf7a1d
Nestor Villafañe Mass <mass.nestor@gmail.com>
2022-07-21 21:21:53 -0500

    Initial commit

---
+++ src/main/java/org/example/Saludo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Saludo {
+
+    public Saludo() {
+        System.out.println("Se crea instancia de saludo");
+    }
+
+    public void imprimirSaludo(String texto){
+        System.out.println(texto);
+    }
+}
